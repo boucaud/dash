7e5a4d54677df0839c2529e0d1442faf6ca0660e
Adrien Boucaud <adrien.boucaud@kitware.com>
2019-12-22 20:40:07 +0100

    Add environment

---
+++ src/Environment.cpp
@@ -0,0 +1,31 @@
+#include "Environment.h"
+
+bool Environment::has(const std::string& name) const
+{
+    return this->globalVariables.count(name) > 0 || this->localVariables.count(name) > 0;
+}
+
+void Environment::set(const std::string& name, const Variable& variable, bool isGlobal)
+{
+    if(name.empty() || variable.getCount() == 0)
+    {
+        return;
+    }
+
+    Environment::VariableMapType& variableMap = isGlobal ? this->globalVariables : this->localVariables;
+    variableMap[name] = variable;
+}
+
+Variable& Environment::get(const std::string& name)
+{
+    if((this->localVariables.count(name) == 0 && this->globalVariables.count(name) == 0))
+    {
+        this->localVariables[name] = Variable();
+        return this->localVariables[name];
+    }
+    if(this->localVariables.count(name))
+    {
+        return this->localVariables[name];
+    }
+    return this->globalVariables[name];
+}
---
+++ src/Environment.h
@@ -0,0 +1,22 @@
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+#include <unordered_map>
+
+#include "Variable.h"
+
+class Environment
+{
+    public:
+    Environment() = default;
+    bool has(const std::string& name) const;
+    void set(const std::string& name, const Variable& variable, bool isGlobal = false);
+    Variable& get(const std::string& name);
+
+    private:
+    using VariableMapType = std::unordered_map<std::string, Variable>;
+    VariableMapType globalVariables;
+    VariableMapType localVariables;
+};
+
+#endif
